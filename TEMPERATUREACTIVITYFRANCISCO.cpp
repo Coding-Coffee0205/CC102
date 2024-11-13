7a863f10f5590e1b1831f50e7eae9073a85264ff
Coding-Coffee0205 <mikiereno277353@gmail.com>
2024-11-13 10:00:08 +0800

    Update TEMPERATUREACTIVITYFRANCISCO.cpp

--- TEMPERATUREACTIVITYFRANCISCO.cpp
+++ TEMPERATUREACTIVITYFRANCISCO.cpp
@@ -8,9 +8,11 @@ int main () {
 
     if (response < 32) {
         cout << "Bring a heavy jacket." << endl;
-    } else if (response == 32 || response <= 50) {
+    } 
+    else if (response == 32 || response <= 50) {
         cout << "Bring a light jacket." << endl;
-    } else if ( response > 50) {
+    } 
+    else if ( response > 50) {
         cout << "No need to bring any jacket." << endl;
     }
     return 0;
